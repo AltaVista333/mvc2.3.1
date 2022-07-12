2c763039ea7515e7e08a74212ccd482b7b18c939
AltaVista <altavista444@yandex.ru>
2022-07-12 21:54:51 +0300

    some result

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -3,17 +3,16 @@ package web.controller;
 
 import java.util.List;
 import java.util.Optional;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
+import javax.validation.ConstraintViolation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import web.model.User;
 import web.services.UserService;
 
@@ -27,37 +26,31 @@ public class UserController {
         this.service = service;
     }
 
-    @ModelAttribute("users")
-    public List<User> add() {
-        return service.getAllUsers();
-    }
-
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String allUsers(ModelMap model) {
-        //model.addAttribute("users", service.getAllUsers());
-        return "users-view";
-    }
-
-    @RequestMapping(value = "/users", method = RequestMethod.POST)
-    public String addOrUpdateUser(
-        @Valid @ModelAttribute(value = "user") User user,
-        BindingResult bindingResult) {
-
-
-            if(!bindingResult.hasErrors()) {
-                Optional.ofNullable(user.getId())
-                    .ifPresentOrElse(
-                        x -> service.updateUserById(user),
-                        () -> service.addUser(user));
-            }
+    @GetMapping("/")
+    public String getUsersTable(Model model,
+        RedirectAttributes redirectAttributes) {
+        model.addAttribute("allUsersTable", service.getAllUsers());
+       // model.addAttribute("errs",  model.getAttribute("errors"));
 
         return "users-view";
     }
 
-    @RequestMapping(value = "/delete", method = RequestMethod.GET)
-    public String deleteUser(@RequestParam Long userId, ModelMap modelMap) {
-        service.deleteUserById(userId);
-        modelMap.addAttribute("users", service.getAllUsers());
-        return "qwe/users-view";
+    @PostMapping("/add")
+    public String addUser(@ModelAttribute @Validated User user,
+        BindingResult result, RedirectAttributes attributes) {
+
+        if (result.hasErrors()) {
+
+            List<String> errors = result.getAllErrors().stream()
+                .map(x -> x.unwrap(ConstraintViolation.class))
+                .map(ConstraintViolation::getMessage).toList();
+            attributes.addFlashAttribute("errors", errors);
+
+        } else {
+            Optional.ofNullable(user.getId())
+                .ifPresentOrElse(x -> service.updateUserById(user),
+                    () -> service.addUser(user));
+        }
+        return "redirect:/";
     }
 }
